b40fcb17ca664837303f0a4dcb05290820280c0a
Dagomara <53799719+Dagomara@users.noreply.github.com>
2022-06-11 11:21:55 -0400

    Update code.js
    API spelling didn't match frontend code

--- assets/js/code.js
+++ assets/js/code.js
@@ -41,7 +41,7 @@ function doLogin()
 			if (this.readyState == 4 && this.status == 200)
 			{
 				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
+				userId = jsonObject.ID;
 				console.log("userID:", userId);
 				if( userId < 1 )
 				{
