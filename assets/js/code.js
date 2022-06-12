7f987d772386f9cb5b6504688d0c2f803021070e
Dagomara <53799719+Dagomara@users.noreply.github.com>
2022-06-12 10:20:30 -0400

    Update code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -274,7 +274,7 @@ if (window.location.href.includes("contacts.html"))
 				toggleContactEdits(cRow);
 				// return to previous state
 				console.log("cancel button confirm -- sends to", $(cRow).data('oldState'));
-				putJSON(cRow, $(cRow).data('oldState'));
+				putJSON($(cRow), $(cRow).data('oldState'));
 				// swap button groups
 				cRow.find(".saveCancelGroup:first").hide();
 				cRow.find(".editInfoGroup:first").show();
